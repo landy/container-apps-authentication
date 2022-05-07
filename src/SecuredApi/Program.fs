4d628c1a01537e8a7bca7bb9d95d7bcc4f5c8ab2
Jiří Landsman <landsman@cngroup.dk>
2022-05-07 07:30:30 +0200

    init

---
+++ src/SecuredApi/Program.fs
@@ -0,0 +1,14 @@
+﻿open Microsoft.AspNetCore.Builder
+open Giraffe
+
+let routes = choose [
+    route "/" >=> text "hello world"
+]
+
+let builder = WebApplication.CreateBuilder()
+builder.Services.AddGiraffe() |> ignore
+
+let app = builder.Build()
+app.UseGiraffe routes
+
+app.Run()
