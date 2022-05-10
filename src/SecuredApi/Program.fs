79d492d8ea0bb98ec811cc386f45d2697167116e
Jiří Landsman <landsman@cngroup.dk>
2022-05-10 22:08:52 +0200

    print user

--- src/SecuredApi/Program.fs
+++ src/SecuredApi/Program.fs
@@ -1,8 +1,14 @@
 ﻿open Microsoft.AspNetCore.Builder
 open Giraffe
+open Microsoft.AspNetCore.Http
+
+let authHandler : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        json ctx.User.Claims next ctx
 
 let routes = choose [
     route "/" >=> text "hello world"
+    route "/auth" >=> authHandler
 ]
 
 let builder = WebApplication.CreateBuilder()
