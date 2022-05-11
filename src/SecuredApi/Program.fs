02b83c50f35b96f9360dc50f56d08d1b8e5b0943
Jiří Landsman <landsman@cngroup.dk>
2022-05-11 13:07:38 +0200

    try

--- src/SecuredApi/Program.fs
+++ src/SecuredApi/Program.fs
@@ -1,20 +1,30 @@
 ﻿open System
+open System.Security.Claims
 open Microsoft.AspNetCore.Builder
 open Giraffe
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Identity.Web
 
+
+type AuthResponse = {
+    Path : string
+    IsAuthenticated: bool
+    Claims : string list
+    Time : string
+    Headers : string list
+} 
+
 let authHandler : HttpHandler =
     fun (next : HttpFunc) (ctx : HttpContext) ->
         let info =
-            {|
-                path = ctx.Request.Path.Value
-                isAuthenticated = ctx.User.Identity.IsAuthenticated
-                claims = ctx.User.Claims
-                time = DateTime.UtcNow.ToString()
-                headers = ctx.Request.Headers
-            |}
+            {
+                Path = ctx.Request.Path.Value
+                IsAuthenticated = ctx.User.Identity.IsAuthenticated
+                Claims = ctx.User.Claims |> List.ofSeq |> List.map (fun c -> $"{c.Type}={c.Value}")
+                Time = DateTime.UtcNow.ToString()
+                Headers = ctx.Request.Headers |> List.ofSeq |> List.map (fun header -> $"{header.Key}={header.Value}")
+            }
         json info next ctx
 
 let routes = choose [
@@ -23,13 +33,24 @@ let routes = choose [
 ]
 
 let builder = WebApplication.CreateBuilder()
-builder.Services.AddGiraffe() |> ignore
 builder.Services.AddAuthorization() |> ignore
-builder.Services.AddAuthentication().AddAppServicesAuthentication() |> ignore
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration.GetSection("AzureAD")) |> ignore
+//builder.Services.AddAuthentication().AddAppServicesAuthentication() |> ignore
+builder.Services.AddGiraffe() |> ignore
 
 let app = builder.Build()
-app.UseGiraffe routes
 app.UseAuthentication() |> ignore
 app.UseAuthorization() |> ignore
+app.UseGiraffe routes
+
+app.MapGet("/auth2", Func<HttpContext,AuthResponse>(fun (ctx:HttpContext) ->
+    {
+        Path = ctx.Request.Path.Value
+        IsAuthenticated = ctx.User.Identity.IsAuthenticated
+        Claims = ctx.User.Claims |> List.ofSeq |> List.map (fun c -> $"{c.Type}={c.Value}")
+        Time = DateTime.UtcNow.ToString()
+        Headers = ctx.Request.Headers |> List.ofSeq |> List.map (fun header -> $"{header.Key}={header.Value}")
+    }
+    )) |> ignore
 
 app.Run()
